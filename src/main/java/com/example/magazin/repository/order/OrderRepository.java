ad03953b4aeadac1dbb88467b3447ae821375a4e
Ярослав <sakhno.yaroslav.13.95@gmail.com>
2023-04-09 04:57:21 +0300

    repository creating

---
+++ src/main/java/com/example/magazin/repository/order/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.example.magazin.repository.order;
+
+import com.example.magazin.entity.order.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+}
